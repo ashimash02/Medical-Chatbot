a3b6a6b261c1d65a8e3f3b615f5637adfbdaf4f3
ashimash02 <sh.ash.sharma2002@gmail.com>
2026-04-01 13:39:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ashima Sharma",
+    author_email="sh.ash.sharma2002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
